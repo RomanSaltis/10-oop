bde07c7d3d1a2b3cd1a6bd571e49162405c45396
RomanSaltis <roman.saltis@gmail.com>
2021-05-20 22:11:21 +0300

    Dogs and Cats

---
+++ index.js
@@ -0,0 +1,10 @@
+const Dog = require('./js/Dog');
+const Cat = require('./js/Cat');
+
+const rexas = new Dog('Rex', 'brown');
+const brisius = new Dog('Brisius', 'grey');
+const rainis = new Cat('Rainis', 'rainas');
+
+rexas.introduce();
+brisius.introduce();
+rainis.introduce();
